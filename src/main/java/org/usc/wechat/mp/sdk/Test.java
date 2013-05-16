800ae0b3b27c0a49fee1969a55dd5eb5c83b005d
lishunli <lishunli.me@gmail.com>
2013-05-16 21:53:02 +0800

    init project

---
+++ src/main/java/org/usc/wechat/mp/sdk/Test.java
@@ -0,0 +1,12 @@
+package org.usc.wechat.mp.sdk;
+
+/**
+ *
+ * @author Shunli
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+
+}
