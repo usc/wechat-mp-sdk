10abb08eade1f838f8fc252c51c9e0cbc9e74ee4
lishunli <lishunli@xunlei.com>
2014-03-17 15:00:50 +0800

    refactoring Http.Get with httpclint Fluent API.

--- src/main/java/org/usc/wechat/mp/sdk/cache/AccessTokenCache.java
+++ src/main/java/org/usc/wechat/mp/sdk/cache/AccessTokenCache.java
@@ -1,14 +1,12 @@
 package org.usc.wechat.mp.sdk.cache;
 
+import java.net.URI;
 import java.util.concurrent.DelayQueue;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.lang3.StringUtils;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.fluent.Request;
 import org.apache.http.client.utils.URIBuilder;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.util.EntityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.usc.wechat.mp.sdk.util.Constant;
@@ -31,47 +29,36 @@ public class AccessTokenCache {
     private final static LoadingCache<License, String> cache = CacheBuilder.newBuilder().build(new CacheLoader<License, String>() {
         @Override
         public String load(License license) throws Exception {
-            URIBuilder uriBuilder = new URIBuilder(Constant.WECHAT_TOKEN_URL);
-            uriBuilder.addParameter("grant_type", GrantType.CLIENT_CREDENTIAL.getValue());
-            uriBuilder.addParameter("appid", license.getAppId());
-            uriBuilder.addParameter("secret", license.getAppSecret());
+            URI uri = new URIBuilder(Constant.WECHAT_TOKEN_URL)
+                    .setParameter("grant_type", GrantType.CLIENT_CREDENTIAL.getValue())
+                    .setParameter("appid", license.getAppId())
+                    .setParameter("secret", license.getAppSecret())
+                    .build();
+            log.info("get access token for {}, url = {}", license, uri);
 
-            log.info("get access token for {}, url = {}", license, uriBuilder);
-            HttpGet httpget = new HttpGet(uriBuilder.build());
-            try {
-                HttpResponse response = HttpClientBuilder.create().build().execute(httpget);
-                if (response == null) {
-                    log.info("http no response when get access token for {}", license);
-                    return null;
-                }
+            String json = Request.Get(uri).execute().returnContent().asString();
+            log.info("get access token for {}, rtn = {}", license, json);
 
-                String json = EntityUtils.toString(response.getEntity());
-                log.info("get access token for {}, rtn = {}", license, json);
-
-                AccessTokeJsonRtn rtn = JSONObject.parseObject(json, AccessTokeJsonRtn.class);
-                if (rtn == null) {
-                    log.info("parse return json msg failed when get access token for {}, rtn = {}", license, json);
-                    return null;
-                }
-
-                JsonRtnUtil.appendErrorHumanMsg(rtn);
-
-                if (StringUtils.isNotEmpty(rtn.getErrCode()) && !StringUtils.equals(Constant.WECHAT_JSON_RTN_SUCCESS_CODE, rtn.getErrCode())) {
-                    log.info("unsuccessfully get access token for {}, rtn = {}", license, rtn);
-                    return null;
-                }
-
-                // for safe, expiresSeconds - 60s
-                int expiresSeconds = Ints.max(rtn.getExpiresIn() - 60, 0);
-                queue.put(new DelayItem<License>(license, TimeUnit.NANOSECONDS.convert(expiresSeconds, TimeUnit.SECONDS)));
-
-                String accessToken = rtn.getAccessToken();
-                log.info("successfully get access token for {}, rtn = {}", license, rtn);
-                return accessToken;
-            } finally {
-                httpget.releaseConnection();
+            AccessTokeJsonRtn rtn = JSONObject.parseObject(json, AccessTokeJsonRtn.class);
+            if (rtn == null) {
+                log.info("parse return json msg failed when get access token for {}, rtn = {}", license, json);
+                return null;
             }
 
+            JsonRtnUtil.appendErrorHumanMsg(rtn);
+
+            if (StringUtils.isNotEmpty(rtn.getErrCode()) && !StringUtils.equals(Constant.WECHAT_JSON_RTN_SUCCESS_CODE, rtn.getErrCode())) {
+                log.info("unsuccessfully get access token for {}, rtn = {}", license, rtn);
+                return null;
+            }
+
+            // for safe, expiresSeconds - 60s
+            int expiresSeconds = Ints.max(rtn.getExpiresIn() - 60, 0);
+            queue.put(new DelayItem<License>(license, TimeUnit.NANOSECONDS.convert(expiresSeconds, TimeUnit.SECONDS)));
+
+            String accessToken = rtn.getAccessToken();
+            log.info("successfully get access token for {}, rtn = {}", license, rtn);
+            return accessToken;
         }
     });
 
